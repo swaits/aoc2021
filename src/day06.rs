c88083930639ff87b963e796f9fe1aa99117c29b
Stephen Waits <steve@waits.net>
2021-12-06 17:27:37 -0700

    day 6 solution

--- src/day06.rs
+++ src/day06.rs
@@ -1,8 +1,24 @@
 use anyhow::Result;
+use cached::proc_macro::cached;
+
+use crate::utils::parse_by_char;
+
+#[cached]
+fn count_babies(birthday: isize, end_day: isize) -> usize {
+    1 + (birthday..=end_day)
+        .skip(2 + 7)
+        .step_by(7)
+        .map(|day| count_babies(day, end_day) as usize)
+        .sum::<usize>()
+}
+
+fn count_population(pop: &Vec<isize>, end_day: isize) -> usize {
+    pop.iter().map(|d| count_babies(d - 8, end_day)).sum()
+}
 
 pub(crate) fn run() -> Result<(usize, usize)> {
-    let input = include_str!("../data/day06.txt");
-    Ok((0, 0))
+    let data = parse_by_char::<isize>(include_str!("../data/day06.txt"), ',')?;
+    Ok((count_population(&data, 80), count_population(&data, 256)))
 }
 
 #[cfg(test)]
@@ -10,12 +26,21 @@ mod tests {
     use super::*;
 
     #[test]
+    fn emit_babies() {
+        assert_eq!(count_babies(3 - 8, 18), 5);
+    }
+
+    #[test]
     fn test_example() {
-        let input = include_str!("../data/day06-test.txt");
+        let data = parse_by_char::<isize>(include_str!("../data/day06-test.txt"), ',').unwrap();
+        assert_eq!(count_population(&data, 18), 26);
+        assert_eq!(count_population(&data, 80), 5934);
+        assert_eq!(count_population(&data, 256), 26984457539);
     }
 
     #[test]
     fn test_my_data() {
-        let input = include_str!("../data/day06.txt");
+        let data = parse_by_char::<isize>(include_str!("../data/day06.txt"), ',').unwrap();
+        assert_eq!(count_population(&data, 80), 358214);
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,17 @@
 use anyhow::Result;
 use std::str::FromStr;
 
+pub fn parse_by_char<T: FromStr>(s: &str, c: char) -> Result<Vec<T>> {
+    let mut ret = Vec::new();
+    for token in s.split(c) {
+        let data = match token.trim().parse::<T>() {
+            Ok(d) => d,
+            Err(_) => panic!("parse error"),
+        };
+        ret.push(data);
+    }
+    Ok(ret)
+}
 pub fn parse_data<T: FromStr>(s: &str) -> Result<Vec<T>> {
     let mut ret = Vec::new();
     for l in s.lines() {
