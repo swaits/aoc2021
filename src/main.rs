feca3062c8712dd4481da22719b0b9ac5fee6dda
Stephen Waits <steve@waits.net>
2021-12-07 19:56:17 -0700

    cleanup main a bit

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,19 @@ mod day07;
 mod utils;
 
 fn main() -> Result<()> {
-    println!("day01: {:?}", day01::run()?);
-    println!("day02: {:?}", day02::run()?);
-    println!("day03: {:?}", day03::run()?);
-    println!("day04: {:?}", day04::run()?);
-    println!("day05: {:?}", day05::run()?);
-    println!("day06: {:?}", day06::run()?);
-    println!("day07: {:?}", day07::run()?);
+    let dayfns = [
+        day01::run,
+        day02::run,
+        day03::run,
+        day04::run,
+        day05::run,
+        day06::run,
+        day07::run,
+    ];
+
+    for (i, f) in dayfns.iter().enumerate() {
+        println!("Day {:02} -> {:?}", i, f()?);
+    }
+
     Ok(())
 }
